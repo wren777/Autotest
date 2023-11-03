a3f33e1df067d7d56a5147b6708843c61b39b341
Vladimir Verkhoturov <46687689+wren777@users.noreply.github.com>
2023-11-03 11:24:10 +0300

    Update order_info_test.py

--- order_info_test.py
+++ order_info_test.py
@@ -1,7 +1,6 @@
 import sender_request
 import data
 
-# Владимир Верхотуров, 8-я когорта - Финальный проект. Инженер по тестированию плюс.
 def test_get_order_info_by_track():
     track = sender_request.create_order(data.order_body).json()['track']
     response = sender_request.get_order_info_by_track(track)
